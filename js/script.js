e005fe5a97dffa8ead10edaaf46ee9c5ea3b2475
Mikhail1365 <fggl12@yandex.ru>
2022-01-10 19:09:05 +0300

    right_navbar ready

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let elem = document.querySelector('.right-navbar__img_1');
+let elemItem1 = document.querySelector('.right-navbar__item_1');
+console.log('elem:', elem);
+console.log(elem.clientWidth);
+console.log(elem.clientHeight);
+let parElem = elem.parentElement;
+console.log('elem parent:', parElem);
+console.log(parElem.clientWidth);
+console.log(parElem.clientHeight);
+console.log(elemItem1);
+
+console.log(elemItem1.clientHeight);
+console.log(elemItem1.clientWidth);
+
