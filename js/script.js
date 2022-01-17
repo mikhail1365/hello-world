7a4735082702079668d37d47dc1e87ffd26333a7
Mikhail1365 <fggl12@yandex.ru>
2022-01-17 22:33:44 +0300

    portfolio_cite

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@
 // console.log(window.pageYOffset);
 
 
-function changeImg(btn, item1, item2){
-	btn.onclick = function() {
+// изменение состояния кнопки для смены новостей (блок 'news)
+function changeImg(btn, item1, item2) {
+	btn.onclick = function () {
 		item1.classList.toggle('display-none');
 		item2.classList.toggle('display-none');
 		btn.classList.toggle('btn-color-active');
@@ -17,16 +18,37 @@ function changeImg(btn, item1, item2){
 		console.log('btn:', btn);
 	}
 	setTimeout(changeImg, 20000, btn, item1, item2);
-
 }
-
-
 
 let btn = document.querySelector('.but-change');
 let worldCupItem = document.querySelector('.world-cup');
 let filmItem = document.querySelector('.film');
 
 setTimeout(changeImg, 2000, btn, worldCupItem, filmItem);
+
+
+
+// изменение состояния бургера и выплывающего меню
+function chngBurgerState() {
+	
+	burger.classList.toggle('menu__burger-active');
+	menu__row.classList.toggle('menu__row-active');
+	header.classList.toggle('header-active');
+	console.log('from func 1:', burger);
+}
+
+let burger = document.querySelector('.menu__burger');
+let menu__row = document.querySelector('.menu__row');
+let header = document.querySelector('.header');
+console.log('burger:', burger);
+console.log('menu__row:', menu__row);
+console.log('header:', header);
+burger.addEventListener('click', chngBurgerState);
+// // setTimeout(chngBurgerState, 2000, burger);
+console.log('burger:', burger);
+// console.log(burger.style.before);
+
+
 
 
 // console.log('worldCupItem:', worldCupItem.classList.contains('item-hidden'));
